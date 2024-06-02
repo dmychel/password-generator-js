c38571ec36ec0a06a40879e9d1539490848f10f7
dylon <dmychel@tutanota.com>
2024-06-02 17:46:50 -0400

    import and rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Footer from "./UI/Footer";
+import Header from "./UI/Header";
+import Form from "./form/Form";
+
 function App() {
-  return;
+  return (
+    <section className="app">
+      <Header />
+      <main>
+        <Form />
+      </main>
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
