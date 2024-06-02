3b0f29db3246c19d06ca3c8dd34dea3af4dc3674
dylon <dmychel@tutanota.com>
2024-06-02 18:40:44 -0400

    jsx update

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -12,51 +12,59 @@ export default function Form() {
   return (
     <section className="password_form">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="min-length">Min Length</label>
-        <select
-          name="min-length"
-          id="min-length"
-          value={min}
-          onChange={(e) => setMin(e.target.value)}
-        >
-          <option value={6}>{6}</option>
-          <option value={7}>{7}</option>
-          <option value={8}>{8}</option>
-        </select>
+        <div>
+          <label htmlFor="min-length">Min Length</label>
+          <select
+            name="min-length"
+            id="min-length"
+            value={min}
+            onChange={(e) => setMin(e.target.value)}
+          >
+            <option value={6}>{6}</option>
+            <option value={7}>{7}</option>
+            <option value={8}>{8}</option>
+          </select>
+        </div>
 
-        <label htmlFor="min-length">Max Length</label>
-        <select
-          name="max-length"
-          id="max-length"
-          value={max}
-          onChange={(e) => setMax(e.target.value)}
-        >
-          <option value={6}>{6}</option>
-          <option value={7}>{7}</option>
-          <option value={8}>{8}</option>
-          <option value={9}>{9}</option>
-          <option value={10}>{10}</option>
-          <option value={11}>{11}</option>
-          <option value={12}>{12}</option>
-        </select>
+        <div>
+          <label htmlFor="min-length">Max Length</label>
+          <select
+            name="max-length"
+            id="max-length"
+            value={max}
+            onChange={(e) => setMax(e.target.value)}
+          >
+            <option value={6}>{6}</option>
+            <option value={7}>{7}</option>
+            <option value={8}>{8}</option>
+            <option value={9}>{9}</option>
+            <option value={10}>{10}</option>
+            <option value={11}>{11}</option>
+            <option value={12}>{12}</option>
+          </select>
+        </div>
 
-        <label htmlFor="special">Special Characters</label>
-        <input
-          type="checkbox"
-          name="special"
-          id="special"
-          value={special}
-          onChange={(e) => setSpecial(e.target.checked)}
-        />
+        <div>
+          <label htmlFor="special">Special Characters</label>
+          <input
+            type="checkbox"
+            name="special"
+            id="special"
+            value={special}
+            onChange={(e) => setSpecial(e.target.checked)}
+          />
+        </div>
 
-        <label htmlFor="numbers">Numbers</label>
-        <input
-          type="checkbox"
-          name="numbers"
-          id="numbers"
-          value={number}
-          onChange={(e) => setNumber(e.target.checked)}
-        />
+        <div>
+          <label htmlFor="numbers">Numbers</label>
+          <input
+            type="checkbox"
+            name="numbers"
+            id="numbers"
+            value={number}
+            onChange={(e) => setNumber(e.target.checked)}
+          />
+        </div>
 
         <input type="submit" value="Generate" />
       </form>
