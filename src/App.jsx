abae23a6e5023bd6ef420e81a9643cda4e0e959d
dylon <dmychel@tutanota.com>
2024-06-02 21:12:40 -0400

    import and rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Footer from "./UI/Footer";
 import Header from "./UI/Header";
 import Form from "./form/Form";
+import Password from "./UI/Password";
 
 function App() {
+  const [formObject, setFormObject] = useState({});
+
   return (
     <section className="app">
       <Header />
       <main>
-        <Form />
+        <Form setFormObject={setFormObject} />
+        <Password formObject={formObject} />
       </main>
       <Footer />
     </section>
